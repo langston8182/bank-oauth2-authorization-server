8818dfda1280bfeae5b69f24c7b71ca19734441f
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-20 00:32:40 -0400

    Configuration cors

--- src/main/java/com/cmarchive/bank/bankoauth2authorizationserver/controller/UserController.java
+++ src/main/java/com/cmarchive/bank/bankoauth2authorizationserver/controller/UserController.java
@@ -3,12 +3,14 @@ package com.cmarchive.bank.bankoauth2authorizationserver.controller;
 import com.cmarchive.bank.bankoauth2authorizationserver.domaine.User;
 import com.cmarchive.bank.bankoauth2authorizationserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
     @Autowired
